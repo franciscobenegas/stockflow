a0a138be9128f436e0b3a62fcef6e433747eeb49
Francisco Benegas <fbenegas@bancard.com.py>
2024-10-24 16:53:24 -0300

    hiden menu lateral

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,19 @@
+"use client";
 import React from "react";
 import { SidebarRoutes } from "../SidebarRoutes";
 import { Logo } from "../Logo";
+import { useSession } from "next-auth/react";
 
 export function Sidebar() {
+  const { data: session } = useSession();
+
   return (
-    <div className="h-screen">
-      <div className="h-full flex flex-col ">
-        <Logo />
-        <SidebarRoutes />
+    <div className={session?.user?.name ? "block" : "hidden"}>
+      <div className="h-screen">
+        <div className="h-full flex flex-col ">
+          <Logo />
+          <SidebarRoutes />
+        </div>
       </div>
     </div>
   );
